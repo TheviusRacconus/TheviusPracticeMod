ff761153aac65f1b8d565bc1fdb85e0d6a1ad18e
TheviusRacconus <TheviusRacconus@users.noreply.github.com>
2015-10-01 21:54:50 -0700

    Working on world interaction

--- src/main/java/com/thevius/theviustest/block/BlockColorful.java
+++ src/main/java/com/thevius/theviustest/block/BlockColorful.java
@@ -1,10 +1,18 @@
 package com.thevius.theviustest.block;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
 public class BlockColorful extends BlockThevius
 {
     public BlockColorful()
     {
         super();
         this.setBlockName("colorfulBlock");
+    }
+
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer entityPlayer, int side, float hitX, float hitY, float hitZ)
+    {
+        return false;
     }
 }
